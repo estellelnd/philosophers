4b393b6c00e99ba0f4fd6283a641a29d00664398
estellelk <estelle.landi0epitech.eu>
2025-09-02 00:33:52 +0200

    [+] init

--- philo/main.c
+++ philo/main.c
@@ -6,21 +6,34 @@
 /*   By: estellek <estellek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 16:06:37 by estellek          #+#    #+#             */
-/*   Updated: 2025/08/12 16:27:12 by estellek         ###   ########.fr       */
+/*   Updated: 2025/09/02 00:33:35 by estellek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void check_arguments(int argc, char **argv)
+void	check_arguments(int argc, char **argv)
 {
-	//check valid integers
-	//maximum value with macros to avoid overflow
-	//only numbers
-	//numbers of meals 
+	// check valid integers
+	// maximum value with macros to avoid overflow
+	// only numbers
+	// numbers of meals
 }
 
-void	init_values(int argc, char **argv);
+void	init_values(int argc, char **argv)
+{
+	int		i;
+	t_philo	*data;
+
+	i = 0;
+	data = malloc(sizeof(t_philo));
+	data->count_philos = ft_atoi(argv[1]);
+	data->time_to_die = ft_atoi(argv[2]);
+	data->time_to_eat = ft_atoi(argv[3]);
+	data->time_to_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+		data->number_of_times_each_philosopher_must_eat = ft_atoi(argv[5]);
+}
 
 int	main(int argc, char **argv)
 {
@@ -32,3 +45,13 @@ int	main(int argc, char **argv)
 		write(2, "Bad arguments\n", 16);
 	return (0);
 }
+
+// parsing positive only + overflows
+// allocate memory for the philosophers and forks
+// init mutexes
+// assign values to each philosopher (circular)
+// print mutex
+// death check loop
+// synchronization
+
+// function for current time
--- philo/philo.h
+++ philo/philo.h
@@ -6,25 +6,26 @@
 /*   By: estellek <estellek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 16:03:51 by estellek          #+#    #+#             */
-/*   Updated: 2025/08/29 00:15:59 by estellek         ###   ########.fr       */
+/*   Updated: 2025/09/02 00:33:24 by estellek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* number_of_philosophers time_to_die time_to_eat time_to_sleep
 [number_of_times_each_philosopher_must_eat] */
 
-//thread = philosophe
-//mutex = fourchette
-//steps : manger, dormir, penser
-//manger avec 2 fourchettes only
-//10 millisecondes = 10 000 microsecondes pour afficher la mort
+// thread = philosophe
+// mutex = fourchette
+// steps : manger, dormir, penser
+// manger avec 2 fourchettes only
+// 10 millisecondes = 10 000 microsecondes pour afficher la mort
 
-/* 
-    arg1 = number_of_philosophers (nombre de philosophes et nombre de fourchettes)
-    arg2 = time_to_die (s’il a pas mangé depuis time_to_die millisecondes il meurt)
-    arg3 = time_to_eat (temps pour manger avec deux fourchettes en millisecondes)
-    arg4 = time_to_sleep (temps pour dormir en milliseconde)
-    arg5 = number_of_times_each_philosopher_must_eat (nombre de fois que chaque philosophe doit manger, arg optionnel)
+/*
+	arg1 = number_of_philosophers (nombre de philosophes et nombre de fourchettes)
+	arg2 = time_to_die (s’il a pas mangé depuis time_to_die millisecondes il meurt)
+	arg3 = time_to_eat (temps pour manger avec deux fourchettes en millisecondes)
+	arg4 = time_to_sleep (temps pour dormir en milliseconde)
+	arg5 = number_of_times_each_philosopher_must_eat (nombre de fois que chaque philosophe doit manger,
+			arg optionnel)
  */
 
 #ifndef PHILO_H
@@ -49,12 +50,17 @@ typedef struct s_philo
 {
 	int				id;
 	int				counter_meals;
+	int				counter_philos;
 	bool			dead;
 	bool			full;
 	long			last_meal_time;
 	t_fork			*left_fork;
 	t_fork			*right_fork;
 	pthread_t		thread_id;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				number_of_times_each_philosopher_must_eat;
 }					t_philo;
 
 int					ft_atoi(char *str);
